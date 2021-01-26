c50d850199831c7c40c35db413d242eb07b5fdc2
DESKTOP-6QC5URD\Bdapps <yeo11200@hanmail.net>
2021-01-26 10:21:16 +0900

    test

---
+++ src/commnt.js
@@ -0,0 +1,24 @@
+console.log('aaaa');
+
+
+exports.toHHMMSS = function (date) {
+    const timeGap = new Date(0, 0, 0, 0, 0, 0, date); 
+
+    const diffDay  = Math.floor(date / (1000 * 60 * 60 * 24)); // 일수       
+    const diffHour = timeGap.getHours();       // 시간 
+    const diffMin  = timeGap.getMinutes();      // 분
+    const diffSec  = timeGap.getSeconds();      // 초
+    
+    return `${diffDay}일${diffHour}시간${diffMin}분${diffSec}초`;
+}
+
+exports.numberFormat = (data) => {
+    if(data==0) return 0;
+    
+    var reg = /(^[+-]?\d+)(\d{3})/;
+    var n = (data + '');
+    
+    while (reg.test(n)) n = n.replace(reg, '$1' + ',' + '$2');
+    
+    return n;
+}
